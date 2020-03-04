910b3f4709e4ec6574e306fb4f54e13bc9ecaf0d
Ugni Blanc <ajo2185@gmail.com>
2020-03-03 19:09:26 -0700

    added functionality and logic to gem calculation

--- assets/game.js
+++ assets/game.js
@@ -5,54 +5,61 @@
 // check if game won, add 1 to wins, reset
 // check if game lost, add 1 to losses, reset
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     var randomNum = 0;
     var winTracker = 0;
     var lossTracker = 0;
     var totalScore = 0;
 
-    function startGame(){
+    function startGame() {
         totalScore = 0
-        randomNum = Math.floor(Math.random() *131) + 19
-        $('.gems').each(function(){
-            var randomGemNum = Math.floor(Math.random() *11) + 1
-            $(this).attr('gemNum', randomGemNum)
+        // creates random number to match
+        randomNum = Math.floor(Math.random() * 131) + 19;
+        $('.gems').each(function () {
+            var randomGemNum = Math.floor(Math.random() * 11) + 1;
+            $(this).attr('gemNum', randomGemNum);
         })
-        console.log('randomNum: ', randomNum )
-        $('#randomNum').text(randomNum)
-        $('#totalScore').text(totalScore)
+        console.log('randomNum: ', randomNum);
+        $('#randomNum').text(randomNum);
+        $('#totalScore').text(totalScore);
+        
     }
 
     startGame()
 
-    $('.gems').on('click', function(){
-        var gemNum = parseInt($(this).attr('gemNum'))
-        console.log(gemNum)
+    // assigns each gem a random value
+    $('.gems').on('click', function () {
+        $('#result').empty();
+        var gemNum = parseInt($(this).attr('gemNum'));
+        totalScore = totalScore + gemNum;
+        console.log(gemNum);
+        console.log(totalScore);
+        checkIfGameWon();
+
     })
+    
 
-    function calculateGems(){
 
+    function checkIfGameWon() {
+        if (totalScore === randomNum) {
+            $("#result").text("You Win!");
+            winTracker++;
+            $("#winTracker").text(winTracker);
+            startGame();
+        }
+
+        else if (totalScore > randomNum) {
+            $("#result").text("You Lost!");
+            lossTracker++;
+            $("#lossTracker").text(lossTracker);
+            startGame();
+        } 
+        else {
+            $('#totalScore').text(totalScore);
+        }
     }
 
-    function checkIfGameWon(){
-        if (totalScore === randomNum) {
-            $("userScore").append ("You Win!");
-            winTracker ++;
-        }
-
-    function checkIfGameLost(){
-        if  (totalScore > randomNum) {
-            $("userScore").append ("You Lost!");
-            lossTracker ++;
-        }
-    }   
-
-
-
-
-
-}
 
 
 })
